f6f2e6e6f3eccc495c26e68df10b8b56b72d1c08
Neerav Patel <niravpatel4848@gmail.com>
2022-01-18 22:59:30 -0600

    Add project files.

---
+++ ExtensionMethodExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is an extension method example.";
+
+            int totalWords = str.CountWords();
+
+            System.Console.WriteLine("Total words in str is {0}",totalWords);
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ ExtensionMethodExample/StringExtMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExample
+{
+    public static class StringExtMethod
+    {
+        public static int CountWords(this string st)
+        {
+            return st.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
